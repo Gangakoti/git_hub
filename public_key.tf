3410fe4800dd1e99b5919b4d879a927d320c64e4
gangs <gangakoti06@gmail.com>
2022-09-07 16:06:12 +0530

    push

---
+++ public_key.tf
@@ -0,0 +1,7 @@
+##create public key
+resource "aws_key_pair" "pub_key" {
+  key_name   = "pub_key" //ganga for gitbash connect 
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCiLomjFFa7RHrda2IoywVFoOXnD5aDCLAj9DGn3i1ToQmD0tGzozoplps4FCTerAsqlCqPyh9c26ZPSma0UKFP3PzMnu8zidlGosdJcVnWTUMmdBYkZWDEuOjdYCYvpn1z3/nRvD5BgSXjBzKQIiXZOpoLve2dvZgfkn2epISeXtkrGNCwlPz9PYIh+MBTeRzK2WLHJvomr5DlkKOwHa33e3ur/R7FibVJ47Egdcs/lASffLltCzVOl6lrZ9tXqwu4vF58zbdEi7dET+zRoAYW3qDquFsoes9AYeEAfYbnHN0SYQDY8R2Hm5L9RFf7LVNunNRiup5JBK/FrNgpbHVNUOPClqzK4ODUYBG+JHGukcnH2NPjnB+SGjI1DXcIPlR0kwAr0By7va5ZdLYVbLTs2YQB3Yc1ipTDo7d4QHvJSZIXAELITHUrASao98+9K3vw4eMT9QZolgqUu8feKnoGHEjbMwN/y3BjSC9jc1w7PCdQgFNek5aQS3UAB1ZEgdc= gangadevi koti@LAPTOP-3E8EB0T5"
+}
+
+hgfgfg
